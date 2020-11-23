db884437acc30e909c1890eff261c4748587851e
huangxiaocao <huangxiaocao@jd.com>
2020-11-23 16:37:31 +0800

    build: react开发环境搭建

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return <div>
+            <h1 className="hello">React开发环境搭建模版</h1>
+        </div>
+    }
+}
+
+export default App;
